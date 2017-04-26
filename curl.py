5b5be8ff06ca177fc2f05a2565630e93b5aa0002
Javier <javialonso007@hotmail.es>
2017-04-26 14:15:52 +0200

    Function for uploading files to file.io
    Simulates "curl" in PHP for uploading files (in this case, music) to temporal server "file.io"

---
+++ curl.py
@@ -0,0 +1,28 @@
+import requests
+import ujson as json
+import unicodedata
+from unidecode import unidecode
+import re
+
+def descarga(chat_id_str,title):
+	full_name="{}_{}.mp3".format(title,chat_id_str)
+	url = 'https://file.io/?expires=1w'
+	files = {'file': open(full_name,'rb')}
+	print("\n\tSubiendo archivo a 'file.io'")
+	link = None
+	n=0
+	while link==None: 												 	 # For ensuring that the file is uploaded correctly
+		response = requests.post(url, files=files)
+		test = response.text
+		print("JSON recibido: ",test)
+		decoded = unidecode(test) 									 	 # It's needed to decode text for avoiding 'bytes' problems (b'<meta...)
+		print("JSON decodificado: ",decoded)
+		if '<html>' in decoded: 									 	 # When upload fails, 'file.io' sends a message with <html> header.
+			print("\n\tFallo al subir el archivo. Reintentando... #",n)	 # If it's detected, assings 'link = None' and then 'while' loop restars
+			link = None
+			n=n+1 													 	 # Little counter
+		else:
+			json_data = json.loads(decoded)
+			link = json_data['link']
+			print("\n\nEnlace de descarga directa: ",link)
+	return link
